59fe37fa03a13e39d33b0460aafd2c8743311340
junsuPark <krpjs0508@gmail.com>
2023-04-28 23:11:08 +0900

    video adapter

---
+++ app/src/main/java/app/junsu/youtube/ui/VideoAdapter.kt
@@ -0,0 +1,69 @@
+package app.junsu.youtube.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import app.junsu.youtube.R
+import app.junsu.youtube.model.Video
+import com.bumptech.glide.Glide
+
+class VideoAdapter : ListAdapter<Video, VideoAdapter.ViewHolder>(diffUtil) {
+    inner class ViewHolder(
+        private val view: View,
+    ) : RecyclerView.ViewHolder(view) {
+        fun bind(
+            item: Video,
+        ) {
+            val imgThumbnail = view.findViewById<ImageView>(R.id.img_video_thumbnail)
+            val tvTitle = view.findViewById<TextView>(R.id.tv_video_title)
+            val tvSubTitle = view.findViewById<TextView>(R.id.tv_video_subtitle)
+
+            Glide.with(
+                imgThumbnail.context,
+            ).load(
+                item.thumb,
+            ).into(
+                imgThumbnail,
+            )
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): ViewHolder = ViewHolder(
+        view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_video, parent, false
+        ),
+    )
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int,
+    ) {
+        return holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<Video>() {
+            override fun areItemsTheSame(
+                oldItem: Video,
+                newItem: Video,
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: Video,
+                newItem: Video,
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
