6639f6b5c1f059deffce65a7d37a1dbf9b187a62
junsuPark <krpjs0508@gmail.com>
2023-04-28 19:58:25 +0900

    video

---
+++ app/src/main/java/app/junsu/youtube/model/Video.kt
@@ -0,0 +1,9 @@
+package app.junsu.youtube.model
+
+data class Video(
+    val title: String,
+    val subtitle: String,
+    val description: String,
+    val thumb: String,
+    val sources: String,
+)
