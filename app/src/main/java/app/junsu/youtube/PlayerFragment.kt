e8ce97c2c6a031c6241d1830d5b9ec5c24fe94a0
junsuPark <krpjs0508@gmail.com>
2023-04-28 19:44:38 +0900

    motionlayout

--- app/src/main/java/app/junsu/youtube/PlayerFragment.kt
+++ app/src/main/java/app/junsu/youtube/PlayerFragment.kt
@@ -1,5 +1,60 @@
 package app.junsu.youtube
 
+import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
+import app.junsu.youtube.databinding.FragmentPlayerBinding
+import kotlin.math.abs
 
-class PlayerFragment : Fragment(R.layout.fragment_player)
+class PlayerFragment : Fragment(R.layout.fragment_player) {
+    private lateinit var binding: FragmentPlayerBinding
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentPlayerBinding.bind(view)
+
+        binding.motionLayoutPlayer.setTransitionListener(
+            object : MotionLayout.TransitionListener {
+                override fun onTransitionStarted(
+                    motionLayout: MotionLayout?,
+                    startId: Int,
+                    endId: Int,
+                ) {
+                }
+
+                override fun onTransitionChange(
+                    motionLayout: MotionLayout?,
+                    startId: Int,
+                    endId: Int,
+                    progress: Float,
+                ) {
+                    binding.let {
+                        (activity as MainActivity).also { mainActivity ->
+                            mainActivity.findViewById<MotionLayout>(
+                                R.id.motion_layout_main,
+                            ).progress = abs(progress)
+                        }
+                    }
+                }
+
+                override fun onTransitionCompleted(
+                    motionLayout: MotionLayout?,
+                    currentId: Int,
+                ) {
+                }
+
+                override fun onTransitionTrigger(
+                    motionLayout: MotionLayout?,
+                    triggerId: Int,
+                    positive: Boolean,
+                    progress: Float,
+                ) {
+                }
+            },
+        )
+    }
+}
